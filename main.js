86eda85e075862c36622308d1e4b2d3b4edca865
dominicdaviescodes <codewithdom@gmail.com>
2020-07-04 11:58:15 +0100

    if guess right all squares match correct color

--- main.js
+++ main.js
@@ -9,12 +9,9 @@ var colors = [
   'rgb(255, 0, 255)',
 ];
 
-// loop through squares
-
 var squares = document.querySelectorAll('.square');
 // the goal color
 var pickedColor = colors[3];
-
 // color displayed in h1 tag
 var colorDisplay = document.getElementById('colorDisplay');
 colorDisplay.textContent = pickedColor;
@@ -25,16 +22,24 @@ for (var i = 0; i < squares.length; i++) {
   // add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
 
-  // add click listeners to squares
   squares[i].addEventListener('click', function () {
     // get color of picked square
     var clickedColor = this.style.backgroundColor;
     // compare color to pickedColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = 'Correct';
+      changeColors(clickedColor);
     } else {
       this.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try Again';
     }
   });
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    // change each square's color to match winning color
+    squares[i].style.backgroundColor = color;
+  }
 }
