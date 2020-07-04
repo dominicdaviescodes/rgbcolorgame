311f1ace9d26e8b241911449320b2118d03c07d9
dominicdaviescodes <codewithdom@gmail.com>
2020-07-04 17:33:38 +0100

    fix issue where in easy mode, the winning color could be one of the hidden 3 squares

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
@@ -11,7 +12,8 @@ var hardBtn = document.querySelector('#hardBtn');
 easyBtn.addEventListener('click', function () {
   hardBtn.classList.remove('selected');
   easyBtn.classList.add('selected');
-  colors = generateRandomColors(3);
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++) {
@@ -25,9 +27,17 @@ easyBtn.addEventListener('click', function () {
 hardBtn.addEventListener('click', function () {
   hardBtn.classList.add('selected');
   easyBtn.classList.remove('selected');
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = 'block';
+  }
 });
 resetButton.addEventListener('click', function () {
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++) {
